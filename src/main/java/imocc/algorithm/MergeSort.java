911feca0c3f931df1177e6c67c4150af50d090a2
ljj <yeshrb@126.com>
2018-01-10 22:19:18 +0800

    Modify MergeSort

--- src/main/java/imocc/algorithm/MergeSort.java
+++ src/main/java/imocc/algorithm/MergeSort.java
@@ -6,8 +6,22 @@ import java.util.Arrays;
  * Created by ljj on 2018/1/7.
  */
 public class MergeSort {
-    private void merge(int[] arr, int l, int mid, int r) {
-        int[] aux = Arrays.copyOfRange(arr, l, r + 1);
+    public void sort(Comparable[] arr) {
+        int n = arr.length;
+        sort(arr,0,n-1);
+    }
+    public void sort(Comparable[] arr, int left, int right) {
+        if(left>=right)
+            return;
+        int mid = (left+right)/2;
+        sort(arr,left,mid);
+        sort(arr,mid+1,right);
+        merge(arr,left,mid,right);
+    }
+
+
+    private void merge(Comparable[] arr, int l, int mid, int r) {
+        Comparable[] aux = Arrays.copyOfRange(arr, l, r + 1);
         // 初始化，i指向左半部分的起始索引位置l；j指向右半部分起始索引位置mid+1
         int i = l, j = mid + 1;
         for (int k = l; k <= r; k++) { // 如果左半部分元素已经全部处理完毕
@@ -18,7 +32,7 @@ public class MergeSort {
             else if(j > r)  {// 如果右半部分元素已经全部处理完毕
                 arr[k] = aux[i-l];
                 i++;
-            } else if(aux[i-l]< aux[j-l]) {// 左半部分所指元素 < 右半部分所指元素
+            } else if(aux[i-l].compareTo(aux[j-l])<0) {// 左半部分所指元素 < 右半部分所指元素
                 arr[k] = aux[i-l];
                 i++;
             }else { // 左半部分所指元素 >= 右半部分所指元素
--- src/test/java/imocc/algorithm/MergeSortTest.java
+++ src/test/java/imocc/algorithm/MergeSortTest.java
@@ -5,43 +5,85 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 
+import static imocc.algorithm.SortTestHelper.isSorted;
 import static org.junit.Assert.*;
 
 /**
  * Created by lee on 2018/1/5.
  */
 public class MergeSortTest {
+
+    private Class<?> targetClass;
+    private Integer[] testData;
+    private static final int SCALE = 1000000;
+    private Object instanceOfTargetClass;
+
     @Before
     public void setUp() throws Exception {
+        targetClass = Class.forName("imocc.algorithm.MergeSort");
+        testData = SortTestHelper.generateRandomArray(SCALE,0,1000000);
+        instanceOfTargetClass = targetClass.newInstance();
     }
 
     @After
     public void tearDown() throws Exception {
     }
-//    @Test
-//    public void testSort() {
-//        int[] a = {8,2,7,6,1,3,5,4};
-//        MergeSort.sort(a);
-//        int[] b = {1,2,3,4,5,6,7,8};
-//        Assert.assertArrayEquals(b,a);
-//    }
+    @Test
+    public void testSort() throws Exception{
+        Method  sort_method = targetClass.getMethod("sort",
+                new Class[]{Comparable[].class});
+        long spanTime = evalMethodSpenTime(sort_method, testData);
+        assertTrue(isSorted(testData));
+
+    }
+    @Test
+    public void testSortIsONlgN() throws Exception {
+        int scale_new = 100000;
+        Integer[] data = SortTestHelper.generateRandomArray(scale_new,0,scale_new);
+        Method  sort_method = targetClass.getMethod("sort",
+                new Class[]{Comparable[].class});
+        long spanTime_new = evalMethodSpenTime(sort_method,data);
+        System.out.println(targetClass.getSimpleName() + ":" + spanTime_new);
+
+        long spanTime = evalMethodSpenTime(sort_method, testData);
+        System.out.println(targetClass.getSimpleName() + ":" + spanTime);
+        System.out.println(spanTime*1.0/spanTime_new);
+        double complexeScale = (Math.log(SCALE))/(Math.log(scale_new)) *(SCALE/scale_new);
+        assertTrue((spanTime/spanTime_new) < complexeScale);
+
+    }
+
+
     @Test
     public void testmerge() throws Exception {
-
-        Class<?> TargetClass = Class.forName("imocc.algorithm.MergeSort");
-        Method  merge_method = TargetClass.getDeclaredMethod("merge",
-                       new Class<?>[]{int[].class, int.class, int.class, int.class});
+        Method  merge_method = targetClass.getDeclaredMethod("merge",
+                       new Class<?>[]{Comparable[].class, int.class, int.class, int.class});
         merge_method.setAccessible(true);
-        Object in = TargetClass.newInstance();
-        int [] a = {2,4,6,8,1,3,5,7};
+        Integer [] a = {2,4,6,8,1,3,5,7};
         Object[] paras = {a,0,3,7};
-        int [] expected = {1,2,3,4,5,6,7,8};
-        merge_method.invoke(in,paras);
+        Integer [] expected = {1,2,3,4,5,6,7,8};
+        InvokeMethod(instanceOfTargetClass,merge_method, paras);
         Assert.assertArrayEquals(expected,a);
 
     }
+
+    private long evalMethodSpenTime(Method sort_method, Object paras)
+            throws IllegalAccessException, InvocationTargetException {
+        long startTime = System.currentTimeMillis();
+        Object[] params = new Object[]{paras};
+        InvokeMethod(instanceOfTargetClass,sort_method, params);
+        long endTime = System.currentTimeMillis();
+        return endTime - startTime;
+    }
+
+    private void InvokeMethod(Object instanceCalled,Method method, Object[] paras)
+            throws IllegalAccessException, InvocationTargetException {
+
+        method.invoke(instanceCalled,paras);
+    }
+
 
 }
