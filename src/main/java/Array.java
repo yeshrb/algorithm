2c24fbd277adcc809ea90a390137f72b72db6fb3
yeshrb <yeshrb@126.com>
2018-10-31 13:22:16 +0800

    add test failed

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -12,10 +12,28 @@ public class Array {
     }
 
     public void addLast(int e) {
-        if(size == data.length) {
+        if (size == data.length) {
             throw new IllegalArgumentException("addLast failed,Array is full");
         }
         data[size] = e;
         size++;
+    }
+
+    public void add(int index, int e) {
+        if (size == data.length) {
+            throw new IllegalArgumentException("add failed,Array is full");
+        }
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("add failed,idnex must be >=0 and <=size");
+        }
+        for (int i = size - 1; i > index + 1; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
+
+    public int getSize() {
+        return this.size;
     }
 }
--- src/test/java/ArrayTest.java
+++ src/test/java/ArrayTest.java
@@ -19,14 +19,34 @@ public class ArrayTest {
         Array arr1 = new Array(20);
         assertNotNull(arr1);
     }
+    @Test
+    public void testGetSize(){
+        Array arr = new Array(20);
+        assertEquals(0,arr.getSize());
+        arr = initTestArray(20);
+        assertEquals(10,arr.getSize());
+
+    }
 
     @Test
     public void testAddLast() {
-        Array arr = new Array();
-        for (int i = 0; i < 10; i++) {
-            arr.addLast(i);
-        }
+        Array arr = initTestArray(10);
         assertArrayEquals(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, arr.data);
     }
 
+    @Test
+    public void testAdd(){
+        Array arr = initTestArray(20);
+        arr.add(3,100);
+        assertArrayEquals(new int[]{0, 1, 100,2, 3, 4, 5, 6, 7, 8, 9}, arr.data);
+
+
+    }
+    private Array initTestArray(int capacity) {
+        Array arr = new Array(capacity);
+        for (int i = 0; i < 10; i++) {
+            arr.addLast(i);
+        }
+        return arr;
+    }
 }
