85b1bbc07dc0fc24207de335fcaef211d3c31a8f
李俊杰 <yeshrb@126.com>
2018-11-01 08:56:49 +0800

    add test finised

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -11,14 +11,6 @@ public class Array {
         this(10);
     }
 
-    public void addLast(int e) {
-        if (size == data.length) {
-            throw new IllegalArgumentException("addLast failed,Array is full");
-        }
-        data[size] = e;
-        size++;
-    }
-
     public void add(int index, int e) {
         if (size == data.length) {
             throw new IllegalArgumentException("add failed,Array is full");
@@ -26,14 +18,43 @@ public class Array {
         if (index < 0 || index > size) {
             throw new IllegalArgumentException("add failed,idnex must be >=0 and <=size");
         }
-        for (int i = size - 1; i > index + 1; i--) {
+        for (int i = size - 1; i >= index; i--) {
             data[i + 1] = data[i];
         }
         data[index] = e;
         size++;
     }
+    public void addLast(int e) {
+        add(size,e);
+    }
+    public void addFirst(int e) {
+        add(0,e);
+    }
 
     public int getSize() {
         return this.size;
+    }
+
+    public int getCapacity() {
+        return data.length;
+    }
+
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Arrat: size = %d, capacity = %d\n",size,data.length));
+        res.append("[");
+        for (int i = 0; i < size; i++) {
+            res.append(i);
+            if(i<size)
+                res.append(',');
+
+        }
+        return super.toString();
     }
 }
--- src/test/java/ArrayTest.java
+++ src/test/java/ArrayTest.java
@@ -1,5 +1,6 @@
 
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,10 +9,7 @@ import static org.junit.Assert.*;
  * Created by ljj on 2018/1/10.
  */
 public class ArrayTest {
-    //    @Test
-//    public void testToString()  {
-//
-//    }
+
     @Test
     public void testCreate() {
         Array arr = new Array();
@@ -19,12 +17,13 @@ public class ArrayTest {
         Array arr1 = new Array(20);
         assertNotNull(arr1);
     }
+
     @Test
-    public void testGetSize(){
+    public void testGetSize() {
         Array arr = new Array(20);
-        assertEquals(0,arr.getSize());
+        assertEquals(0, arr.getSize());
         arr = initTestArray(20);
-        assertEquals(10,arr.getSize());
+        assertEquals(10, arr.getSize());
 
     }
 
@@ -35,13 +34,51 @@ public class ArrayTest {
     }
 
     @Test
-    public void testAdd(){
+    public void testAdd() {
         Array arr = initTestArray(20);
-        arr.add(3,100);
-        assertArrayEquals(new int[]{0, 1, 100,2, 3, 4, 5, 6, 7, 8, 9}, arr.data);
-
+        arr.add(3, 100);
+        assertArrayEquals(new int[]{0, 1, 2, 100, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0}, arr.data);
 
     }
+
+    @Test
+    public void testGetCapacity() {
+        Array arr = initTestArray(10);
+        assertEquals(10, arr.getCapacity());
+        assertEquals(10, arr.getSize());
+        Array arr1 = initTestArray(20);
+        assertEquals(20, arr1.getCapacity());
+        assertEquals(10, arr1.getSize());
+    }
+
+    @Test
+    public void testAddFirst() {
+        Array arr = new Array();
+        arr.addFirst(100);
+        assertEquals(100, arr.data[0]);
+        arr.addFirst(200);
+        assertEquals(200, arr.data[0]);
+
+    }
+
+    @Test
+    public void testIsEmpty() {
+        Array arr = new Array();
+        assertTrue(arr.isEmpty());
+        arr.addLast(1);
+        assertFalse(arr.isEmpty());
+
+    }
+
+    @Test
+    public void testToString() {
+        Array arr = initTestArray(10);
+        String expected = "Arrat: size = 10, capacity = 20\n"
+                + "[0,1,2,3,4,5,6,7,8,9]";
+        assertEquals(expected,arr.toString());
+
+    }
+
     private Array initTestArray(int capacity) {
         Array arr = new Array(capacity);
         for (int i = 0; i < 10; i++) {
