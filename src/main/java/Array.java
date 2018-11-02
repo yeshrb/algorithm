fea78ab4534222e756b493b65154b2f886ff0719
李俊杰 <yeshrb@126.com>
2018-11-02 22:32:34 +0800

    2-5 数组，包含，搜索和删除

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -24,11 +24,13 @@ public class Array {
         data[index] = e;
         size++;
     }
+
     public void addLast(int e) {
-        add(size,e);
+        add(size, e);
     }
+
     public void addFirst(int e) {
-        add(0,e);
+        add(0, e);
     }
 
     public int getSize() {
@@ -47,11 +49,11 @@ public class Array {
     @Override
     public String toString() {
         StringBuilder res = new StringBuilder();
-        res.append(String.format("Arrat: size = %d, capacity = %d\n",size,data.length));
+        res.append(String.format("Arrat: size = %d, capacity = %d\n", size, data.length));
         res.append("[");
         for (int i = 0; i < size; i++) {
             res.append(i);
-            if(i<size-1)
+            if (i < size - 1)
                 res.append(',');
         }
         res.append("]");
@@ -59,7 +61,7 @@ public class Array {
     }
 
     public int get(int index) {
-        if(index < 0 || index >=size){
+        if (index < 0 || index >= size) {
             throw new IllegalArgumentException("get failed,index must >=0 and < size");
         }
 
@@ -67,9 +69,47 @@ public class Array {
     }
 
     public void set(int index, int e) {
-        if(index < 0 || index >=size){
+        if (index < 0 || index >= size) {
             throw new IllegalArgumentException("set failed,index must >=0 and < size");
         }
         data[index] = e;
+    }
+
+    public int find(int e) {
+        int res = -1;
+        for (int i = 0; i < data.length - 1; i++) {
+            if (data[i] == e) {
+                res = i;
+                break;
+            }
+        }
+        return res;
+    }
+
+    public int remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("remove failed,index must >=0 and < size");
+        }
+        int res = data[index];
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        size--;
+        return res;
+    }
+
+    public int removeFirst() {
+        return remove(0);
+    }
+
+    public int removeLast() {
+        return remove(size - 1);
+    }
+
+    public void removeElement(int e) {
+        int index = find(e);
+        if(index != -1)
+            remove(index);
+
     }
 }
--- src/test/java/ArrayTest.java
+++ src/test/java/ArrayTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
 public class ArrayTest {
     Array arr;
     @Rule
-    public ExpectedException thrown= ExpectedException.none();
+    public ExpectedException thrown = ExpectedException.none();
 
     @Before
     public void setUp() {
@@ -95,14 +95,61 @@ public class ArrayTest {
     }
 
     @Test
-    public void testSet(){
+    public void testSet() {
         Array arr = initTestArray(20);
-        arr.set(0,101);
-        assertEquals(101,arr.get(0));
+        arr.set(0, 101);
+        assertEquals(101, arr.get(0));
         thrown.expect(IllegalArgumentException.class);
-        arr.set(arr.getSize(),101);
+        arr.set(arr.getSize(), 101);
         thrown.expect(IllegalArgumentException.class);
-        arr.set(-1,101);
+        arr.set(-1, 101);
+    }
+
+    // 查找数组中元素e所在的索引，如果不存在元素e，则返回-1
+    @Test
+    public void find_should_return_index_of_element_or_neg_1_if_not_exist() {
+        Array arr = initTestArray(20);
+        assertEquals(3, arr.find(3));
+    }
+
+
+    @Test
+    public void remove_should_delete_element_of_index_and_return_element_deleted() {
+        Array arr = initTestArray(20);
+        assertEquals(1, arr.remove(1));
+        assertEquals(9, arr.getSize());
+        assertEquals(2, arr.get(1));
+
+    }
+
+    // 从数组中删除第一个元素, 返回删除的元素
+    @Test
+    public void removeFirst_should_remove_the_first_element_and_return_it() {
+        Array arr = initTestArray(20);
+        assertEquals(0, arr.removeFirst());
+        assertEquals(1, arr.removeFirst());
+        assertEquals(2, arr.removeFirst());
+
+    }
+
+    // 从数组中删除最后一个元素, 返回删除的元素
+    @Test
+    public void removeLast_should_remove_the_last_element_and_return_it() {
+        Array arr = initTestArray(20);
+        assertEquals(9, arr.removeLast());
+        assertEquals(8, arr.removeLast());
+        assertEquals(7, arr.removeLast());
+
+    }
+
+    // 从数组中删除元素e
+    @Test
+    public void removeElement_should_remove_the_element(){
+        Array arr = initTestArray(20);
+        arr.removeElement(1);
+        assertEquals(-1,arr.find(1));
+        arr.removeElement(2);
+        assertEquals(-1,arr.find(2));
     }
 
     private Array initTestArray(int capacity) {
